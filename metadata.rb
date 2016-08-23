e11103230e285c8173facffd33806a019615a558
joseph.bauser <joseph.bauser@calero.com>
2016-08-23 11:13:54 -0400

    Fix #27 - Remove deprecations and warnings
    This commit works to clear out deprecated code as well
    as remove some warnings that get omitted as a result of
    updates.
    
    It resolves the following deprecations and warnings:
    
     * windows_package deprecated in windows cookbook
     * node.set deprecated to be removed in Chef 14
     * Warning about depecatd methods overriding providers from chefspec
    
    In order to support these fixes, the cookbook now requires
    at least chef 12.6 in order to use the new package resource.

--- metadata.rb
+++ metadata.rb
@@ -2,10 +2,14 @@ name             'seven_zip'
 maintainer       'Shawn Neal'
 maintainer_email 'sneal@sneal.net'
 license          'Apache 2.0'
+source_url       'https://github.com/daptiv/seven_zip'
+issues_url       'https://github.com/daptiv/seven_zip/issues'
 description      'Installs/Configures the 7-zip file archiver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 ver_path = File.join(File.dirname(__FILE__), 'version.txt')
 version File.exist?(ver_path) ? IO.read(ver_path).chomp : '2.0.0'
 supports         'windows'
+
+chef_version     '>= 12.6'
 
 depends          'windows', '>= 1.2.2'
